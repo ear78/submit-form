8e40a6fc6ad285cf30acbaa5dd97d5a817f31c77
elliot richardson <ear78@aol.com>
2016-12-08 22:58:41 -0600

    worked on finishing touches, validation can be updated at a later date for specificity

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,27 @@
 $(document).ready(function(){
   "use strict";
 
+
   $(".contact-form").submit(function(event){
     var $required = $(".required");
     var $requiredMessage = $(".required-message");
     var $fname = $("#fname");
     var $lname = $("#lname");
     var $email = $("#email");
-    var $contactFormRequired = [$fname, $lname, $email];
+    var $message = $("#message");
+    var $contactFormRequired = [$fname, $lname, $email, $message];
 
     for(var i = 0; i < $contactFormRequired.length; i++){
       var reqField = $contactFormRequired[i].val();
       if(reqField.length === 0){
+        console.log(reqField);
         event.preventDefault();
         $requiredMessage.removeClass("hide-required-message");
         $requiredMessage.addClass("show-required-message");
         $required.parent().addClass("required-styles");
+
       }
+      
     } // close the for loop
 
   })// close the submit event
